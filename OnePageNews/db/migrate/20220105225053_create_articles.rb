ef227240ee6a5d398f5d5dd9832b2169ff971862
Shami <shamitomita@gmail.com>
2022-01-05 18:00:40 -0500

    trying to fix things :(

--- OnePageNews/db/migrate/20220105225053_create_articles.rb
+++ OnePageNews/db/migrate/20220105225053_create_articles.rb
@@ -3,7 +3,11 @@ class CreateArticles < ActiveRecord::Migration[6.1]
     create_table :articles do |t|
       t.string :title
       t.string :author
+      t.string :news_org
       t.string :category
+      t.string :published
+      t.integer :user_id
+      t.string :description
 
       t.timestamps
     end
--- OnePageNews/db/schema.rb
+++ OnePageNews/db/schema.rb
@@ -10,12 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_04_225910) do
+ActiveRecord::Schema.define(version: 2022_01_05_225053) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "author"
+    t.string "news_org"
     t.string "category"
+    t.string "published"
+    t.integer "user_id"
+    t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
