319b47916af990b47557c8dc558340c85fee44ec
Shami <shamitomita@gmail.com>
2022-01-11 17:02:54 -0500

    DOM listening

---
+++ js-frontend/src/index.js
@@ -0,0 +1,12 @@
+
+document.addEventListener("DOMContentLoaded", () =>{
+  console.log("Ive been Loaded!")
+})
+
+index.addEventListener("click", ()=>{
+  console.log("Ive been clicked!")
+})
+
+toggle.addEventListener("click", ()=>{
+  console.log("Ive been clicked!")
+})
