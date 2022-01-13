f11133d3299aa635257486f6f2faf8510059ddde
Shami <shamitomita@gmail.com>
2022-01-12 19:19:50 -0500

    connected frontend and backend

--- OnePageNews/app/models/article.rb
+++ OnePageNews/app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
   has_many :favorited_articles
   has_many :users, through: :favorited_articles
+
+
+  
 end
--- js-frontend/src/index.js
+++ js-frontend/src/index.js
@@ -1,7 +1,26 @@
+const endPoint = "http://localhost:3000/api/v1/articles"
 
 document.addEventListener("DOMContentLoaded", () =>{
   console.log("Ive been Loaded!")
+  getArticles();
 })
+
+function getArticles(){
+  fetch(endPoint)
+  .then(response => response.json())
+  .then(articles=>{
+    console.log(articles);
+    articles.data.forEach((article) => {
+      const articleMarkup = `
+      <li id=${article.attributes.id}>
+        <img src=${article.attributes.image_url}>
+        <a href=${article.attributes.url}>${article.attributes.title}</a>
+      </li>`
+      document.querySelector('#articles').innerHTML += articleMarkup
+    });
+    document.getElementById('#articles').style.overflow = "scroll";
+  })
+}
 
 index.addEventListener("click", ()=>{
   console.log("Ive been clicked!")
