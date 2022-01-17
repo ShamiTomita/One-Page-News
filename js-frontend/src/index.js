8343a383bd7f07698943b31b4facc16bb7a866a2
Shami <shamitomita@gmail.com>
2022-01-17 11:33:22 -0500

    nav bar css/html good to go

--- js-frontend/src/index.js
+++ js-frontend/src/index.js
@@ -15,10 +15,10 @@ function getArticles(){
     articles.data.forEach((article) => {
       let id = article.id
       const articleMarkup = `
-      <li id=${article.id}>
+      <div id=${article.id}>
         <img src=${article.attributes.image_url}>
         <a id=${article.id} class="article-link" href=${article.attributes.url} onclick="fetchDisplay(${id})" >${article.attributes.title}</a>
-      </li>`
+      </div>`
       document.querySelector('#articles').innerHTML += articleMarkup
       stopLink();
 
@@ -44,13 +44,18 @@ function toggleDisplay(){
   let toggle = document.querySelector("body > div.row > div.column.middle > div.column.toggle")
   let back = document.querySelector("#myTopnav > a:nth-child(2)")
   let middleContent = document.querySelector("body > div.row > div.column.middle > div.middleContent")
+  let toggledBar = document.querySelector("#toggledBar")
+  let myTopnav = document.querySelector("#myTopnav")
   if (toggle.style.display === "none"){
     toggle.style.display = "block";
+    toggledBar.style.display ="inline-block"
     middleContent.style.display = "none"
-
+    myTopnav.style.display = "none"
   }else {
     toggle.style.display = "none";
+    toggledBar.style.display = "none"
     middleContent.style.display = "block"
+    myTopnav.style.display = "inline-block"
   }
 }
 
@@ -71,3 +76,6 @@ function fetchDisplay(articleId){
     toggleDisplay();
   })
   }
+function toggleNavBar(){
+
+}
