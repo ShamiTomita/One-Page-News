967174615f074a5f579871a4670010a2073d5d9b
ShamiTomita <shamitomita@gmail.com>
2022-04-14 22:18:52 -0400

    update

--- OnePageNews/app/serializers/article_serializer.rb
+++ OnePageNews/app/serializers/article_serializer.rb
@@ -1,5 +1,3 @@
-class ArticleSerializer
-  include FastJsonapi::ObjectSerializer
-  attributes :is_top, :category, :title, :author, :news_org, :published, :description, :url, :image_url, :content, :users
-
+class ArticleSerializer < ActiveModel::Serializer
+  attributes :id, :title, :author, :news_org, :published, :description, :content, :url, :image_url
 end
