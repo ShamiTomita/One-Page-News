4509df473af2dadd85c330c8f094ec4356a5a557
ShamiTomita <shamitomita@gmail.com>
2022-04-17 17:58:00 -0400

    update

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () =>{
 
 function getArticles(){
   let counter = 0
-  fetch("https://onepagenews.herokuapp.com/api/v1/articles", {mode: 'cors'})
+  fetch("https://onepagenews.herokuapp.com/api/v1/articles")
   .then(response => response.json())
   .then(articles=>{
 
