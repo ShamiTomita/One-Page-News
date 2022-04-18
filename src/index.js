791dd0e3f60f626147650cdfc0822d84b874dc6c
ShamiTomita <shamitomita@gmail.com>
2022-04-18 10:05:13 -0400

    xxxyyzzz

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ document.addEventListener("DOMContentLoaded", () =>{
 
 function getArticles(){
   let counter = 0
-  fetch($`{API_ROOT}/articles`)
+  fetch("https://onepagenews.herokuapp.com/api/v1/articles")
   .then(response => response.json())
   .then(articles=>{
 
